c64ba70c0194ad0b450b74d0b8c98c8f0ff7fe0b
SiddMan2020 <siddman2020@gmail.com>
2026-06-01 13:19:24 -0500

    fixed ShooterSubsystem

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsystems/ShooterSubsystem.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/subsystems/ShooterSubsystem.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.robotcontroller.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
