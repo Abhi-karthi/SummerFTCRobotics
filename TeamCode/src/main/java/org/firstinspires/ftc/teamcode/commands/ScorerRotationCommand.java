08254a9c26fc4a356f86a9ddff67f76bd5b6a797
SiddMan2020 <siddman2020@gmail.com>
2026-06-01 16:20:40 -0500

    removed comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ScorerRotationCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ScorerRotationCommand.java
@@ -4,9 +4,6 @@ import com.seattlesolvers.solverslib.command.CommandBase;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import org.firstinspires.ftc.teamcode.subsystems.ScorerRotationSubsystem;
 
-/**
- * Command to control the scorer's rotation using the gamepad's right stick X axis.
- */
 public class ScorerRotationCommand extends CommandBase {
     private final ScorerRotationSubsystem rotationSubsystem;
     private final GamepadEx gamepad;
@@ -19,8 +16,6 @@ public class ScorerRotationCommand extends CommandBase {
 
     @Override
     public void execute() {
-        // Map stick input (-1 to 1) to servo range (0 to 1)
-        // Stick left (-1) -> 0.0, Center (0) -> 0.5, Stick right (1) -> 1.0
         double stickX = gamepad.getRightX();
         double servoPosition = (stickX + 1.0) / 2.0;
         
