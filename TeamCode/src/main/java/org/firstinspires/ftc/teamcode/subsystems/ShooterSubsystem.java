6b39f005fc91045bc018cf15b7d789db511ea694
SiddMan2020 <siddman2020@gmail.com>
2026-06-01 13:48:30 -0500

    finished ShooterSubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
@@ -1,12 +1,20 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
 
-public class ShooterSubsystem {
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class ShooterSubsystem extends SubsystemBase {
     IMU imu;
     DcMotor shooter1, shooter2, backLeft, backRight;
+
+
+    private Telemetry telemetry;
 
     public ShooterSubsystem(HardwareMap hardwareMap, IMU imu_) {
         shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
@@ -20,6 +28,23 @@ public class ShooterSubsystem {
         backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        this.imu = imu;
+        this.imu = imu_;
+        this.telemetry = telemetry;
+
+        register();
     }
+
+    @Override
+    public void periodic() {
+        telemetry.addData("shooter1MotorPower", getShooter1MotorPower());
+        telemetry.addData("shooter2MotorPower", getShooter2MotorPower());
+    }
+
+    public void resetIMU() {
+        imu.resetYaw();
+    }
+
+    public double getShooter1MotorPower() { return shooter1.getPower(); }
+    public double getShooter2MotorPower() { return shooter2.getPower(); }
+
 }
