d88cfd564b18a75d82ef2ec1d42619392896aaae
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2026-06-01 13:38:04 -0500

    updated mecanum drive subsystem with correct functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDriveSubsystem.java
@@ -11,11 +11,13 @@ import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-public class MecanumDriveSubsystem {
+public class MecanumDriveSubsystem extends SubsystemBase{
     IMU imu;
     DcMotor frontLeft, frontRight, backLeft, backRight;
 
-    public MecanumDriveSubsystem(HardwareMap hardwareMap, IMU imu_) {
+    private final Telemetry telemetry;
+
+    public MecanumDriveSubsystem(HardwareMap hardwareMap, IMU imu_, Telemetry telemetry) {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
@@ -33,6 +35,49 @@ public class MecanumDriveSubsystem {
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         this.imu = imu;
+        this.telemetry = telemetry;
+
+        register();
     }
+
+    @Override
+    public void periodic() {
+        telemetry.addData("backLeftMotorPower", getBackLeftMotorPower());
+        telemetry.addData("backRightMotorPower", getBackRightMotorPower());
+        telemetry.addData("frontLeftMotorPower", getFrontLeftMotorPower());
+        telemetry.addData("frontRightMotorPower", getFrontRightMotorPower());
+    }
+
+    public void drive(double x, double y, double rotation) {
+        double headingRadians = -(imu.getRobotYawPitchRollAngles().getYaw()) * Math.PI / 180.0;
+
+        double rotatedX = x * Math.cos(headingRadians) - y * Math.sin(headingRadians);
+        double rotatedY = x * Math.sin(headingRadians) + y * Math.cos(headingRadians);
+
+        double frontLeftPower = rotatedY + rotatedX + rotation;
+        double frontRightPower = rotatedY - rotatedX - rotation;
+        double backLeftPower = rotatedY - rotatedX + rotation;
+        double backRightPower = rotatedY + rotatedX - rotation;
+
+        double max = Math.max(1.0, Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower)))));
+        frontLeftPower /= max;
+        frontRightPower /= max;
+        backLeftPower /= max;
+        backRightPower /= max;
+
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+    }
+
+    public void resetIMU() {
+        imu.resetYaw();
+    }
+
+    public double getBackLeftMotorPower() { return backLeft.getPower(); }
+    public double getBackRightMotorPower() { return backRight.getPower(); }
+    public double getFrontLeftMotorPower() { return frontLeft.getPower(); }
+    public double getFrontRightMotorPower() { return frontRight.getPower(); }
 
 }
