761fe78563f406fbe6d7e6090c1ff3c81e3fa5c7
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2026-06-01 16:38:04 -0500

    fixed constants file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
@@ -1,5 +1,20 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot.LogoFacingDirection;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot.UsbFacingDirection;
+
 public class Constants {
-//    public
+    DcMotor intake1 = hardwareMap.get(DcMotor.class, "intake1");
+    DcMotor intake2 = hardwareMap.get(DcMotor.class, "intake2");
+    DcMotor shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
+    DcMotor shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
+
+
+    public static final double CENTER = 0.5;
+    public static final double LEFT_LIMIT = 0.0;
+    public static final double RIGHT_LIMIT = 1.0;
+
 }
