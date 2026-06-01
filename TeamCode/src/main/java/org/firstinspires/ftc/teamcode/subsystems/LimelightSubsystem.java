2953be0f7c9b6021a372040f3b867edd395e45be
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2026-06-01 16:30:52 -0500

    added limelight subsystem constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightSubsystem.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class LimelightSubsystem {
+
+    private final Limelight3A limelight;
+    private final Telemetry telemetry;
+
+    public LimelightSubsystem()
+
 }
