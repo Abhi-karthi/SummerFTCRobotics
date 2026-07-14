7469cf987e315016e0a6344abeb28ed19c413d3e
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2026-07-14 09:33:16 -0500

    created practice subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PracticeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PracticeSubsystem.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-public class PracticeSubsystem {
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class PracticeSubsystem extends SubsystemBase {
+    private DcMotor exampleMotor;
+    private Telemetry telemetry;
+
+    public PracticeSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        exampleMotor = hardwareMap.get(DcMotor.class, "exampleMotor1");
+        this.telemetry = telemetry;
+    }
+
+    @Override
+    public void periodic() {
+        telemetry.addData("Motor Power", getExampleMotorPower());
+    }
+
+    public double getExampleMotorPower() {
+        return exampleMotor.getPower();
+    }
+
+    public void setExampleMotorPower(double power) {
+        exampleMotor.setPower(power);
+    }
 }
